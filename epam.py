7a6fbe0fae04c3bfec6da4741e93864b5a84e02e
shubham joshi <shubham.joshi1507@gmail.com>
2025-02-10 10:11:21 +0530

    Create epam.py
    core python interview

---
+++ epam.py
@@ -0,0 +1,23 @@
+1. what is the differnce between process and thread.
+2.what is asyncio
+3. what is decorator? how its implemented in python, give an example by code and how one implemented in project?
+
+4. what is normalization in db?
+5. what is indexing and how to implement it?
+
+
+6. Have you used lambda function? have you used if so what are pros and cons of it?
+
+7. what is differnce between git merge and rebase
+8. what is cherrypick in git
+
+9. coding round. 
+
+    Maximum Number of Meetings That Can Be Attended
+Problem:
+Find the maximum number of non-overlapping meetings that can be attended.
+
+10. What is difference between rdbms and object-oriented db
+
+
+
